c0db934ddabd24565386ee1846cf0032ca6412eb
hasandemirr <hasan.demir.0396@gmail.com>
2022-06-09 16:03:44 +0300

    Pong Oyunu

---
+++ Pong Oyunu/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Pong_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playGround = new System.Windows.Forms.Panel();
+            this.brick = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.point_lbl = new System.Windows.Forms.Label();
+            this.gameover_lbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.brick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playGround
+            // 
+            this.playGround.Controls.Add(this.gameover_lbl);
+            this.playGround.Controls.Add(this.point_lbl);
+            this.playGround.Controls.Add(this.score_lbl);
+            this.playGround.Controls.Add(this.ball);
+            this.playGround.Controls.Add(this.brick);
+            this.playGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playGround.Location = new System.Drawing.Point(0, 0);
+            this.playGround.Name = "playGround";
+            this.playGround.Size = new System.Drawing.Size(800, 450);
+            this.playGround.TabIndex = 0;
+            // 
+            // brick
+            // 
+            this.brick.BackColor = System.Drawing.Color.Black;
+            this.brick.Location = new System.Drawing.Point(286, 427);
+            this.brick.Name = "brick";
+            this.brick.Size = new System.Drawing.Size(200, 20);
+            this.brick.TabIndex = 0;
+            this.brick.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.DarkMagenta;
+            this.ball.Location = new System.Drawing.Point(225, 237);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.score_lbl.Location = new System.Drawing.Point(12, 9);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(163, 55);
+            this.score_lbl.TabIndex = 2;
+            this.score_lbl.Text = "Score:";
+            this.score_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // point_lbl
+            // 
+            this.point_lbl.AutoSize = true;
+            this.point_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.point_lbl.Location = new System.Drawing.Point(168, 9);
+            this.point_lbl.Name = "point_lbl";
+            this.point_lbl.Size = new System.Drawing.Size(51, 55);
+            this.point_lbl.TabIndex = 3;
+            this.point_lbl.Text = "0";
+            // 
+            // gameover_lbl
+            // 
+            this.gameover_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gameover_lbl.Location = new System.Drawing.Point(321, 131);
+            this.gameover_lbl.Name = "gameover_lbl";
+            this.gameover_lbl.Size = new System.Drawing.Size(133, 136);
+            this.gameover_lbl.TabIndex = 4;
+            this.gameover_lbl.Text = "Game Over!\r\n\r\nF1-Restart\r\n\r\nESC-Exit\r\n";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playGround);
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playGround.ResumeLayout(false);
+            this.playGround.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.brick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playGround;
+        private System.Windows.Forms.PictureBox brick;
+        private System.Windows.Forms.Label gameover_lbl;
+        private System.Windows.Forms.Label point_lbl;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pong Oyunu/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int points = 0;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            Cursor.Hide(); // İmleç gizlendi
+            timer1.Enabled = true; // Kronomotre aktifleşti
+            this.FormBorderStyle = FormBorderStyle.None; // Uygulamanın çerçevesini kaldırıyor.
+            this.TopMost = true; // Uygulama en üstte görünür.
+            this.Bounds = Screen.PrimaryScreen.Bounds; // Tüm ekranı kaplatıyor.
+
+            brick.Top = playGround.Bottom - (playGround.Bottom / 20); //Raketin konumunu alta sabitliyor.
+
+            gameover_lbl.Left = ((playGround.Width / 2) - (gameover_lbl.Width / 2)); //Bitiş ekranını yatay olarak ortalıyor
+            gameover_lbl.Top = ((playGround.Height / 2) - (gameover_lbl.Height / 2)); //Bitiş ekranını dikey olarak ortalıyor
+            gameover_lbl.Visible = false; // Bitiş ekranını görünmez yapıyor.
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            brick.Left = Cursor.Position.X - (brick.Width / 2); // Raketin orta noktası mousun x eksenindeki konumuna eşitlendi
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+            if (ball.Left <= playGround.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= playGround.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <= playGround.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Bottom >= playGround.Bottom)
+            {
+                gameover_lbl.Visible = true;
+                timer1.Enabled = false;
+            }
+
+            if (ball.Bottom >= brick.Top && ball.Bottom <= brick.Bottom && ball.Left >= brick.Left && ball.Right <= brick.Right)
+                //topla raketin birbiriyle temas ettiğini kontrol eden sorgu
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;
+                
+                points += 1;
+                point_lbl.Text = points.ToString();
+
+                Random r = new Random();
+                playGround.BackColor = Color.FromArgb(r.Next(155, 255), r.Next(155, 255), r.Next(155, 255));
+
+                // her temastan sonra topun rengi değişiyor, hızı ve score artıyor
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode ==Keys.Escape)
+            {
+                this.Close();
+            }
+            if(e.KeyCode==Keys.F1)
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                point_lbl.Text = "0";
+                timer1.Enabled = true;
+                gameover_lbl.Visible = false;
+                playGround.BackColor = Color.White;
+            }
+        }
+    }
+}
