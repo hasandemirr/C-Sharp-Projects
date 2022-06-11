d01a09a605daba32a9b10cb2a74ed9d9316fb2b7
hasandemirr <hasan.demir.0396@gmail.com>
2022-06-11 13:10:17 +0300

    Youtube Channel

---
+++ Youtube Channel/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Youtube_Channel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxVideoAdı = new System.Windows.Forms.TextBox();
+            this.textBoxKategori = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLink = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.buttonÇıkış = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(867, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(321, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube Channel";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonÇıkış);
+            this.groupBox1.Controls.Add(this.buttonKaydet);
+            this.groupBox1.Controls.Add(this.textBoxLink);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxKategori);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxVideoAdı);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 350);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yeni Vİdeo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(295, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(301, 350);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İzleme Listesi";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.webBrowser1);
+            this.groupBox3.Location = new System.Drawing.Point(602, 118);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(277, 350);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ekran";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(295, 325);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 22);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(271, 325);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Video Adı";
+            // 
+            // textBoxVideoAdı
+            // 
+            this.textBoxVideoAdı.Location = new System.Drawing.Point(98, 49);
+            this.textBoxVideoAdı.Name = "textBoxVideoAdı";
+            this.textBoxVideoAdı.Size = new System.Drawing.Size(173, 26);
+            this.textBoxVideoAdı.TabIndex = 1;
+            // 
+            // textBoxKategori
+            // 
+            this.textBoxKategori.Location = new System.Drawing.Point(98, 103);
+            this.textBoxKategori.Name = "textBoxKategori";
+            this.textBoxKategori.Size = new System.Drawing.Size(173, 26);
+            this.textBoxKategori.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kategori";
+            // 
+            // textBoxLink
+            // 
+            this.textBoxLink.Location = new System.Drawing.Point(98, 157);
+            this.textBoxLink.Name = "textBoxLink";
+            this.textBoxLink.Size = new System.Drawing.Size(173, 26);
+            this.textBoxLink.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Link";
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(133, 199);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(138, 32);
+            this.buttonKaydet.TabIndex = 6;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // buttonÇıkış
+            // 
+            this.buttonÇıkış.Location = new System.Drawing.Point(6, 312);
+            this.buttonÇıkış.Name = "buttonÇıkış";
+            this.buttonÇıkış.Size = new System.Drawing.Size(265, 32);
+            this.buttonÇıkış.TabIndex = 7;
+            this.buttonÇıkış.Text = "Çıkış";
+            this.buttonÇıkış.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(891, 480);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonÇıkış;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.TextBox textBoxLink;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxVideoAdı;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Youtube Channel/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Youtube_Channel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Youtube_Channel;Integrated Security=True");
+        //Sql Bağlantısı oluşturuldu
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Table_Youtube(Ad,Kategori,Link) values (@P1,@P2,@P3)",baglanti);
+            // Yeni bir komut tanımlandı
+            // Yeni Komut tablodaki yerlere atama yapıyor.
+
+            komut.Parameters.AddWithValue("@P1", textBoxVideoAdı.Text);
+            komut.Parameters.AddWithValue("@P2", textBoxKategori.Text);
+            komut.Parameters.AddWithValue("@P3", textBoxLink.Text);
+            //İlgili Textboxlardaki veriler çekildi
+
+            komut.ExecuteNonQuery();
+            //Komut execute edildi
+
+            baglanti.Close();
+
+            MessageBox.Show("Yeni Video Kaydedildi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+        }
+
+        void videolar()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select*from Table_Youtube",baglanti);
+            // Tablodaki verilerin tamamını çekildi
+
+            DataTable dt = new DataTable();
+            // dt isimli bir tablo datası üretildi
+
+            da.Fill(dt);
+            // Sorgudan gelen veriler dt ye eklendi
+
+            dataGridView1.DataSource = dt;
+            // dt gride eşitlendi
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videolar();
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            // Gridde secilen hücrenin satır bilgisini alıyor
+
+            string link = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            // Satır bilgisindeki link değeri alınıyor
+
+            webBrowser1.Navigate(link);
+            //Link Webbrowser da açılıyor
+
+        }
+    }
+}
